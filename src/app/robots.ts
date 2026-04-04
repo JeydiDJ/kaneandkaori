6430482d610bcab59b42f1166bfcbbdff54bb0a6
JeydiDJ <110451068+JeydiDJ@users.noreply.github.com>
2026-04-04 11:29:24 +0800

    Renamed sitemap file for Google Console Resubmission

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/checkout", "/cart", "/studio", "/studio/", "/api/"],
       },
     ],
-    sitemap: `${getSiteUrl()}/sitemap.xml`,
+    sitemap: `${getSiteUrl()}/sitemap_index.xml`,
   };
 }
