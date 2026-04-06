6e29291850ddf094f4936e4d8b2099854fd8ab75
JeydiDJ <110451068+JeydiDJ@users.noreply.github.com>
2026-04-07 00:12:43 +0800

    Implemented Vercel Website analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 ﻿import type { Metadata } from "next";
 
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 import favicon from "@/assets/favicon.ico";
 import { Footer } from "@/components/layout/Footer";
@@ -82,6 +83,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <Footer />
           </div>
         </CartProvider>
+        <Analytics />
       </body>
     </html>
   );
